d5db56357ccff114e3d0ef5eddfc8d2e72935abb
leandroleekrap <leandroleekrap@gmail.com>
2024-03-07 16:10:35 -0300

    tipagem - tipo string

--- project/test.ts
+++ project/test.ts
@@ -1,2 +1,14 @@
+let pedido: string = "Pizza";
+pedido = "Dener Troquatte";
 
-console.log("Test file is running...");
+let numero: number = 123;
+numero = 321;
+
+let deuBom: boolean = false;
+deuBom = true;
+
+const fun = (value: string): string => {
+  return value;
+};
+
+console.log(fun("Dener Troquatte 1234"));
